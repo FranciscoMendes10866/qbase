93d9fba322d94ceb034832e16de1a47e5370ffa5
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2021-04-15 20:49:04 +0100

    server - self invoking function

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,12 @@ import boom from '@hapi/boom'
 import app from './app'
 
 const { PORT } = process.env
-const port = PORT || 7000
+const port = PORT || 7000;
 
-const bootstrap = async () => {
+(async () => {
   try {
     await app.listen(port)
   } catch (err) {
     throw boom.boomify(err)
   }
-}
-bootstrap()
+})()
